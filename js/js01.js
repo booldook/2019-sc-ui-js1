9476d293b82279a7a378c2dd0a7124b6b5ec357e
booldook <booldook@gmail.com>
2019-03-05 11:38:44 +0900

    Language

--- js/js01.js
+++ js/js01.js
@@ -1,3 +1,21 @@
+/*
+Programming Language(프로그래밍 언어)
+	. Web/FrontEnd
+		- Javascript
+	. Web/BackEnd
+		- PHP
+		- Java
+		- c#
+		- Python
+	. Application
+		- Java
+		- Swift 
+		- Python
+		- VisualBasic
+		- C
+		- C++
+		- c#
+*/
 document.getElementById("bt1").addEventListener("click", function(){
 	alert("Hello Real World");
 	document.getElementById("cmt").innerHTML = "현실세계를 선택!";
@@ -7,3 +25,5 @@ $("#bt2").click(function(){
 	alert("Hello Virtual World");
 	$("#cmt").html("가상현실을 선택!");
 });
+
+
