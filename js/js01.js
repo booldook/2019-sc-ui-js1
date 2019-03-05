78a7fdc21f5a72769cad17018b856262756c8e5b
booldook <booldook@gmail.com>
2019-03-05 11:10:30 +0900

    js01.js

--- js/js01.js
+++ js/js01.js
@@ -1,8 +1,9 @@
-/*
 document.getElementById("bt1").addEventListener("click", function(){
-	alert("Hello World");
+	alert("Hello Real World");
+	document.getElementById("cmt").innerHTML("현실세계를 선택!");
 });
-*/
-$("#bt1").click(function(){
-	alert("Hello World");
+
+$("#bt2").click(function(){
+	alert("Hello Virtual World");
+	$("#cmt").html("가상현실을 선택!");
 });
