bd36ce1cc232a132ac974603ecaf9b2000a63682
booldook <booldook@gmail.com>
2019-03-06 13:49:02 +0900

    if

--- js/js02.js
+++ js/js02.js
@@ -8,6 +8,7 @@ var v2 = document.getElementById("val2");
 var calc = document.getElementById("calc");
 var bt1 = document.getElementById("bt1");
 var result = document.getElementById("result");
+var btReset = document.getElementById("bt_reset");
 
 // form요소일 경우 name으로 접근할 수 있다.
 f = document.form1;
@@ -16,6 +17,7 @@ v2 = f.val2;
 calc = f.calc;
 bt1 = f.bt1;
 result = f.result;
+btReset = f.bt_reset;
 
 // jQuery - id를 이용한 접근법 
 var $f = $("#form1");
@@ -24,6 +26,7 @@ var $v2 = $("#val2");
 var $calc = $("#calc");
 var $bt1 = $("#bt1");
 var $result = $("#result");
+var $btReset = $("#bt_reset");
 
 // jQuery - name을 이용한 접근법 1 
 $f = $("form[name='form1']");
@@ -32,6 +35,7 @@ $v2 = $('input[name="val2"]', $f);
 $calc = $("select[name='calc']", $f);
 $bt1 = $("button[name='bt1']", $f);
 $result = $("input[name='result']", $f);
+$btReset = $("button[name='bt_reset']", $f);
 
 // jQuery - name을 이용한 접근법 2 
 $f = $("form[name='form1']");
@@ -40,20 +44,49 @@ $v2 = $f.find('input[name="val2"]');
 $calc = $f.find("select[name='calc']");
 $bt1 = $f.find("button[name='bt1']");
 $result = $f.find("input[name='result']");
+$btReset = $f.find("button[name='bt_reset']");
 
+/*
 console.log($f);
 console.log($v1);
 console.log($v2);
 console.log($calc);
 console.log($bt1);
 console.log($result);
+*/
+
+// 비교연산자
+// ==(같은 문자인지 비교), ===(같은문자, 같은type), <, >, <=, >= 
+// 함수 안에 있는 변수는 지역변수, 함수 밖에 있는 변수는 전역변수
+
+// if문 - 제어문
+var x = 13;
+if(x > 0) {
+	console.log("양수 입니다.");
+}
+else if(x === 0) {
+	console.log("0 입니다.");
+}
+else if(x < 0) {
+	console.log("음수 입니다.");
+}
+else {
+	console.log("문자 입니다.");
+}
+
 
 // Simple 사칙연산 - JS
-bt1.addEventListener("click", function() {
+bt1.addEventListener("click", function(){
 	var x = v1.value;	//10
 	var y = v2.value;	//20
 	var c = calc.value;	//*
 	result.value = eval(x + c + y);
+});
+btReset.addEventListener("click", function(){
+	v1.value = "";
+	v2.value = "";
+	calc.value = "";
+	result.value = "";
 });
 
 
