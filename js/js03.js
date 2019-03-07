821a1a617e11af55176d527432857f56f27e030e
booldook <booldook@gmail.com>
2019-03-07 10:11:57 +0900

    js03 완료

--- js/js03.js
+++ js/js03.js
@@ -3,4 +3,25 @@ $("#bt_calc").click(function(){
 	var $v2 = $("#val2");
 	var $calc = $("#calc");
 	var $result = $("#result");
+	var v1 = $v1.val();
+	var v2 = $v2.val();
+	var c = $calc.val();
+	
+	if(v1 == ""){
+		alert("값을 입력하셔야 계산이 실행됩니다.");
+		$v1.focus();
+		return;
+	}
+	if(v2 == ""){
+		alert("값을 입력하셔야 계산이 실행됩니다.");
+		$v2.focus();
+		return;
+	}
+	if(c == ""){
+		alert("연산자가 선택되어야 계산이 실행됩니다.");
+		$calc.focus();
+		return;
+	}
+	$result.val(eval(v1 + c + v2));
+	//console.log(			eval(v1 + c + v2)			);
 });
