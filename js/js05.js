6f6ab675be3d0aabf8a3bee4b8c8b33bf37c0fe7
booldook <booldook@gmail.com>
2019-03-07 14:33:43 +0900

    js05

--- js/js05.js
+++ js/js05.js
@@ -1,6 +1,9 @@
 $(".thumbs img").click(function(){
 	//console.log(	$(this).attr("src")	);
 	var src = $(this).attr("src");
-	var t = '<img src="' + src + '" class="img">';
+	var t = '<img src="'+src+'" class="img">';
 	$(".pic_b").html(t);
 });
+
+var src = "../img/a.jpg";
+var t = '<img src="'+src+'" class="img">';
