276bfe8a9ae14a155f21b05553e8ac7e4be5c62a
booldook <booldook@gmail.com>
2019-03-07 15:06:24 +0900

    js05

--- js/js05.js
+++ js/js05.js
@@ -1,9 +1,16 @@
+/*
 $(".thumbs img").click(function(){
 	//console.log(	$(this).attr("src")	);
 	var src = $(this).attr("src");
 	var t = '<img src="'+src+'" class="img">';
 	$(".pic_b").html(t);
 });
+*/
 
-var src = "../img/a.jpg";
-var t = '<img src="'+src+'" class="img">';
+$(".thumbs img").mouseenter(function(){
+	var src = $(this).attr("src");
+	var t = '<img src="'+src+'" class="img">';
+	$(".pic_b").html(t);
+});
+$(".thumbs img").eq(0).trigger("mouseenter");
+
