a98d961f40c47637634e0646ce42bc8a515c1b27
booldook <booldook@gmail.com>
2019-03-08 11:47:03 +0900

    js06

--- js/js06.js
+++ js/js06.js
@@ -7,12 +7,12 @@ $(".btns").children(".btn").not(".btn-danger").click(function(){
 	var n = $(this).index();
 	//$(".box").eq(n).fadeToggle(1000);
 	//animate({애니메이션값}, 속도(숫자), "linear/swing", function(){});
-	$(".box").eq(n).animate({"left":"90%"}, 1000, "swing", function(){
-		$(this).animate({"left": 0}, 500);
+	$(".box").eq(n).stop().animate({"left":"90%", "width":"400px", "borderWidth":"20px"}, 1000, "swing", function(){
+		$(this).stop().animate({"left":0, "width":"100px", "borderWidth":"5px"}, 500);
 	});
 });
 
 $(".btns").children(".btn-danger").click(function(){
 	$(".box").show(0);
-	$(".box").animate({"left":"0"}, 300);
+	$(".box").stop().animate({"left":"0", "width":"100px", "borderWidth":"5px"}, 500);
 });
