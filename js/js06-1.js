8e046a0979a7c984e3181e5a66a176cc4de30848
booldook <booldook@gmail.com>
2019-03-08 13:50:07 +0900

    js06-1

---
+++ js/js06-1.js
@@ -0,0 +1,14 @@
+/***** 프로그래밍에 앞서 알아야 할 지식들 *****/
+// 반복문 for(초기값; 조건; 증가값){}
+// i = i + 1 => i++
+// sum = sum + i => sum += i
+/*
+for(var i=1, sum=0; i<=10; i++){
+	sum += i; 
+}
+console.log(sum);
+*/
+
+
+
+var horse = '<div class="horse"><img src="../img/horse.png" class="img"></div>';
